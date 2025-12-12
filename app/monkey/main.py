634cdc0e776af37c615d35aba5a7d26282c13ea4
Ciprian Ticu <ciprianticu@gmail.com>
2025-12-12 16:52:45 +0200

    update main.py

--- app/monkey/main.py
+++ app/monkey/main.py
@@ -13,7 +13,6 @@ def count_chars(text):
 
 if __name__ == "__main__":
     # Example usage
-    # test
     sample_text = "Python is awesome!"
     result = count_chars(sample_text)
     print(f"The string '{sample_text}' has {result} characters.")
